388a269113ff2da5c12e96570dadc1939c003ae1
codyblank4211 <110508477+codyblank4211@users.noreply.github.com>
2022-08-02 21:37:43 -0400

    Create make_a_mad_lib.py

---
+++ make_a_mad_lib.py
@@ -0,0 +1,28 @@
+month_1 = input("Enter a month: ")
+number_1 = input("Enter a number:  ")
+place_1 = input("Enter a place: ")
+
+noun_1 = input("Enter a noun: ")
+noun_2 = input("Enter a noun: ")
+adjective_1 = input("Enter an adjective: ")
+
+adverb_1 = input("Enter an adverb: ")
+verb_1  = input("Enter a verb: ")
+animal_1 = input("Enter an animal: ")
+
+animal_2 = input("Enter an animal: ")
+adjective_2 = input("Enter an adjective: ")
+adjective_3 = input("Enter an adjective: ")
+
+verb_2  = input("Enter a verb ending in ing: ")
+verb_3  = input("Enter a verb ending in ing: ")
+
+verb_4  = input("Enter a verb: ")
+noun_3 = input("Enter a noun: ")
+
+print(f'Every {month_1} {number_1} we always have a picnic at {place_1}. \
+We invite {noun_1} and {noun_2}. It is next to a very {adjective_1} lake. \
+We have to walk {adverb_1} to get there without {verb_1}. There are {animal_1} \
+and {animal_2} which are very {adjective_2}. We eat lots of food that everyone {adjective_3}. \
+The games are my favorite part. We play {verb_2} and {verb_3} until we can’t walk anymore. \
+Then we {verb_4} back and relax under the old shade tree until {noun_3} says, “Happy Birthday!”')
